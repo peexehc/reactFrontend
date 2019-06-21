01c119bc94dc8c85e7cd1ade29b3de80abc4d8c0
Douglas Garbim Jr <garbim.douglas@gmail.com>
2019-06-21 00:05:43 -0300

    First web app done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Header.css'
 import logo from '../assets/logo.svg'
 import camera from '../assets/camera.svg'
@@ -7,8 +8,12 @@ export default function Header() {
   return (
     <header id="main-header">
       <div className="header-content">
-        <img src={logo} alt="InstaClone" />
-        <img src={camera} alt="Enviar publicação" />
+        <Link to="/">
+          <img src={logo} alt="InstaClone" />
+        </Link>
+        <Link to="/new">
+          <img src={camera} alt="Enviar publicação" />
+        </Link>
       </div>
     </header>
   )
